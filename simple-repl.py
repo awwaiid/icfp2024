d682791bdf29752e7413998f4f1c461d9729ce54
Brock Wilcox <awwaiid@thelackthereof.org>
2024-06-28 11:30:00 -0400

    make repl work

--- simple-repl.py
+++ simple-repl.py
@@ -9,17 +9,19 @@ from subprocess import Popen, PIPE, STDOUT
         # stdout_data = p.communicate(input='data_to_write')[0]
 def main():
     while True:
-        # echo english | python icfp_parser --deode | curl -H 'Authorization: Bearer 9272ed8c-a55f-4dbd-b50f-2970446a6c09' -X POST -d - https://boundvariable.space/communicate | python icfp_parser --decode
-        # english = input(">>> ")
-        # print(f"english: {english}")
-        # # Send this to the stdin of the encoder
-        # p = Popen(['python', 'icfp_parser.py', '--encode'], stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True)
-        # encoded = p.communicate(input=english)[0]
-        # print(f"encoded: {encoded}")
-        # # Send this to the stdin of the server
-        # p = Popen(['curl', '-H', 'Authorization: Bearer 9272ed8c-a55f-4dbd-b50f-2970446a6c09', '-X', 'POST', 'https://boundvariable.space/communicate'], stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True)
-        # response = p.communicate(input=encoded)[0]
-        response = 'SJ!23%}%22/2n}O.%80%#4%$}?I@'
+        english = input(">>> ")
+        print(f"english: {english}")
+        # Send this to the stdin of the encoder
+        p = Popen(['python', 'icfp_parser.py', '--encode'], stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True)
+        encoded, error = p.communicate(input=english)
+        if error:
+            print(f"Error: {error}")
+        print(f"encoded: {encoded}")
+        # Send this to the stdin of the server
+        p = Popen(['curl', '-H', 'Authorization: Bearer 9272ed8c-a55f-4dbd-b50f-2970446a6c09', '-X', 'POST', '-d', '@-', 'https://boundvariable.space/communicate'], stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True)
+        response, error = p.communicate(input=encoded)
+        if error:
+            print(f"Error: {error}")
         print(f"Response: {response}")
         # Send this to the stdin of the decoder
         p = Popen(['python', 'icfp_parser.py', '--decode'], stdout=PIPE, stdin=PIPE, stderr=PIPE, text=True)
